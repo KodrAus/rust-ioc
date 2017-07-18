81d5af4cd605a3a46f91d47e18213b824913b1b5
Ashley Mannix <ashleymannix@live.com.au>
2017-07-18 19:47:39 +1000

    playing with impl trait closures

--- factories/src/main.rs
+++ factories/src/main.rs
@@ -1,32 +1,31 @@
 #![feature(conservative_impl_trait)]
 
-struct GetProduct {
-    id: i32
-}
-
 struct Product {
+    id: i32,
     title: String
 }
 
-trait GetProductHandler {
-    fn get_product(&self, action: GetProduct) -> Result<Product, String>;
-}
-
+#[derive(Clone)]
 struct DbConnection {
 
 }
 
-// A db connection can be a `GetProductHandler`
-impl GetProductHandler for DbConnection {
-    fn get_product(&self, _action: GetProduct) -> Result<Product, String> {
-        Ok(Product {
-            title: "Some product".into()
-        })
+impl DbConnection {
+    fn do_stuff(&self) {
+
     }
 }
 
-// Any function can also be a `GetProductHandler`
-impl<F> GetProductHandler for F 
+struct GetProduct {
+    id: i32
+}
+
+trait GetProductQuery {
+    fn get_product(&self, action: GetProduct) -> Result<Product, String>;
+}
+
+// Any function can be a `GetProductQuery`
+impl<F> GetProductQuery for F 
     where F: Fn(GetProduct) -> Result<Product, String>
 {
     fn get_product(&self, action: GetProduct) -> Result<Product, String> {
@@ -34,24 +33,66 @@ impl<F> GetProductHandler for F
     }
 }
 
-// An example of returning a closure as a handler implementation
-// This could take `DbConn` as an argument instead of another handler. But there's no reason not to take other handlers.
-fn get_product<TGetProduct>(get: TGetProduct) -> impl GetProductHandler
-    where TGetProduct: GetProductHandler
+// Return a closure to get a product
+fn get_product(conn: DbConnection) -> impl GetProductQuery {
+    move |action: GetProduct| {
+        conn.do_stuff();
+
+        Ok(Product {
+            id: action.id,
+            title: "Some product".into()
+        })
+    }
+}
+
+struct SetProductTitle {
+    id: i32,
+    title: String
+}
+
+trait SetProductTitleCommand {
+    fn set_product_title(&self, action: SetProductTitle) -> Result<(), String>;
+}
+
+// Any function can be a `SetProductTitleCommand`
+impl<F> SetProductTitleCommand for F 
+    where F: Fn(SetProductTitle) -> Result<(), String>
 {
-    move |action| {
-        let product = get.get_product(action)?;
+    fn set_product_title(&self, action: SetProductTitle) -> Result<(), String> {
+        self(action)
+    }
+}
 
-        // Do another thing
+// Return a closure to get a product
+// Could this be made more ergonomic?
+fn set_product_title<TGetProduct>(conn: DbConnection, get_product: TGetProduct) -> impl SetProductTitleCommand 
+    where TGetProduct: GetProductQuery
+{
+    move |action: SetProductTitle| {
+        let mut product = get_product.get_product(GetProduct { id: action.id })?;
+        product.title = action.title;
 
-        Ok(product)
+        conn.do_stuff();
+
+        Ok(())
     }
 }
 
 fn main() {
+    let conn = DbConnection {};
+
     // Separate construction from action.
-    let handler = get_product(DbConnection {});
+    let query = get_product(conn.clone());
   
     // Execute the action with the inputs
-    handler.get_product(GetProduct { id: 1 }).unwrap();
+    query.get_product(GetProduct { id: 1 }).unwrap();
+    
+    let command = set_product_title(conn.clone(), query);
+
+    command.set_product_title(SetProductTitle { id: 1, title: "A new title".into() });
+
+    // An alternative implementation of `GetProductQuery` that doesn't use a db connection
+    let command = set_product_title(conn.clone(), |action| Ok(Product { id: 1, title: "Stuff".into() }));
+
+    command.set_product_title(SetProductTitle { id: 1, title: "A new title".into() });
 }
